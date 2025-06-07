004c51ff7826d2c1bece4a988fb2e2fecc2c014e
Elouan Grimm <elouan.grimm@gmail.com>
2025-06-07 18:57:34 +0200

    overhaul

--- scripts/roller.js
+++ scripts/roller.js
@@ -3,7 +3,7 @@ const rollers = document.querySelectorAll('[data-roller]');
 rollers.forEach((roller) => {
     const optionsString = roller.dataset.roller ?? '';
     if (!optionsString) return;
-    const options = optionsString.split(', ');
+    const options = optionsString.split('; ');
     if (!options) return;
 
     roller.innerHTML = options[Math.floor(Math.random() * options.length)];
